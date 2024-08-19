ba85e2fb35d8e366349aef24fee7447f0567b5d0
juliewww <julie.www333@gmail.com>
2024-08-19 17:13:42 +0800

    Remove unused import

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,8 +1,6 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
