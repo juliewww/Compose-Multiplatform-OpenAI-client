adf890c0e834ccf48d2b9f7f09477dfec3c93033
juliewww <julie.www333@gmail.com>
2024-09-06 10:31:40 +0800

    rename settingsModule

--- composeApp/src/commonMain/kotlin/juliewww/compose/multiplatform/openai/client/di/settingsModule.kt
+++ composeApp/src/commonMain/kotlin/juliewww/compose/multiplatform/openai/client/di/settingsModule.kt
@@ -3,6 +3,6 @@ package juliewww.compose.multiplatform.openai.client.di
 import com.russhwolf.settings.Settings
 import org.koin.dsl.module
 
-val SettingsModule = module {
+val settingsModule = module {
     Settings()
 }
