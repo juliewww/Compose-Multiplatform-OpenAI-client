413d512a1d6882456391e8f9b81a8e3b0e1dd660
juliewww <julie.www333@gmail.com>
2024-09-06 09:35:59 +0800

    Add SettingsViewModel.kt

---
+++ composeApp/src/commonMain/kotlin/juliewww/compose/multiplatform/openai/client/SettingsViewModel.kt
@@ -0,0 +1,44 @@
+package juliewww.compose.multiplatform.openai.client
+
+import UserDataRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
+
+class SettingsViewModel(
+    private val userDataRepository: UserDataRepository,
+) {
+    val settingsUiState: StateFlow<SettingsUiState> =
+        userDataRepository.userData
+            .map { userData ->
+                SettingsUiState.Success(
+                    settings = UserEditableSettings(
+                        openAiKey = userData.openAiKey,
+                    ),
+                )
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = WhileSubscribed(5.seconds.inWholeMilliseconds),
+                initialValue = SettingsUiState.Loading,
+            )
+
+    suspend fun setOpenAiKey(newKey: String) {
+        viewModelScope.launch {
+            userDataRepository.setOpenAiKey(newKey)
+        }
+    }
+}
+
+data class UserEditableSettings(
+    val openAiKey: String,
+)
+
+sealed interface SettingsUiState {
+    data object Loading : SettingsUiState
+    data class Success(val settings: UserEditableSettings) : SettingsUiState
+}
