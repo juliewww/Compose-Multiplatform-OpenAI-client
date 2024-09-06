cea5a156da91037bd841910e8d849aa916dc1772
juliewww <julie.www333@gmail.com>
2024-09-06 10:31:54 +0800

    Add modules

--- composeApp/src/commonMain/kotlin/juliewww/compose/multiplatform/openai/client/App.kt
+++ composeApp/src/commonMain/kotlin/juliewww/compose/multiplatform/openai/client/App.kt
@@ -2,7 +2,11 @@ package juliewww.compose.multiplatform.openai.client
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import juliewww.compose.multiplatform.openai.client.di.dispatchModule
 import juliewww.compose.multiplatform.openai.client.di.openAIRepositoryModule
+import juliewww.compose.multiplatform.openai.client.di.preferenceDataSourceModule
+import juliewww.compose.multiplatform.openai.client.di.settingsModule
+import juliewww.compose.multiplatform.openai.client.di.userDataRepositoryModule
 import juliewww.compose.multiplatform.openai.client.di.viewModelModule
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
@@ -14,6 +18,10 @@ fun App() {
         modules(
             viewModelModule,
             openAIRepositoryModule,
+            userDataRepositoryModule,
+            dispatchModule,
+            preferenceDataSourceModule,
+            settingsModule,
         )
     }) {
         androidx.compose.material3.MaterialTheme {
